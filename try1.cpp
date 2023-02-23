9722e73d0e56920dadca609ae11a9891592134ff
kshatracheck <ankitchauhan.kshatrainfotech@gmail.com>
2023-02-23 19:19:59 +0530

    Date 23/02 aaj ka ho gaya

--- try1.cpp
+++ try1.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
 using namespace std;
 
+class parent{
+    public:
+        virtual void display(){
+            
+        }
+};
+
+parent* ptr_parent;
+
+class a{
+    public:
+        void display(){
+            cout << "This is from class A" << endl;
+        }
+};
+
+a obj_a;
+
+class b : public a{
+    public:
+        void display(){
+            cout << "This is from class B" << endl;
+        }
+};
+
+b obj_b;
+
 int main(){
-	cout << "hello blah ";
+   ptr_parent = &obj_a;
+   ptr_parent->display();
 }
